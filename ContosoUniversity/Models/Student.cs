952af1494f9d8a5f639475170608cf69f54ed9c9
jhgudleik <jhgudleik@gmail.com>
2026-04-26 14:22:46 +0300

    Добавьте файлы проекта.

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        // Navigation properties
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
